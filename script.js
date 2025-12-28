04c68c60a7b8a57c2bfd5d28a9c8cca9636f876c
minty-vc <lyceumdudin@gmail.com>
2025-12-28 15:17:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function endGame(type) {
   if (type === "win") {
     const promo = generatePromo();
     resultTextEl.textContent = `Умница! Вот твой промокод: ${promo}`;
-    tgBtn.href =E = "https://t.me/tictictacbot?start=promo_${promo}";
+    tgBtn.href =E = "https://t.me/tictictacbot";
 
 const boardEl = document.getElem
     tgBtn.classList.remove("hidden");
